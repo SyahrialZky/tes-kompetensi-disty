f1dc04ed8fb39e43e2fa586ff9302011b687d459
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-09-27 10:42:23 +0700

    chore: route

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -2,7 +2,7 @@
     <div class="sidebar-logo">
         <!-- Logo Header -->
         <div class="logo-header" data-background-color="dark">
-            <a href="/" class="logo">
+            <a href="/dashboard" class="logo">
                 <span class="text-white">Klinik Sehat</span>
             </a>
             <div class="nav-toggle">
